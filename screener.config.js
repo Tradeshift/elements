55ab15eaefd0e671902e5e7dfcfdb39d2a68d101
Daniel Spitzer <dsp@tradeshift.com>
2019-02-13 12:49:31 +0100

    WIP more screener conf

--- screener.config.js
+++ screener.config.js
@@ -2,7 +2,30 @@ module.exports = {
 	projectRepo: 'Tradeshift/elements',
 	storybookConfigDir: '.storybook',
 	storybookStaticDir: './',
+	storybookApp: 'html',
+	storybookVersion: 4,
 	apiKey: process.env.SCREENER_API_KEY,
 	resolution: '1024x768',
-	excludeRules: [/^(Gallery|Attributes)$/]
+	excludeRules: [/(Gallery|Attributes)$/],
+	browserStack: {
+		username: process.env.BROWSERSTACK_USERNAME,
+		accessKey: process.env.BROWSERSTACK_KEY,
+		maxConcurrent: 5
+	},
+	browsers: [
+		{ browserName: 'chrome' },
+		{ browserName: 'firefox' },
+		{
+			browserName: 'internet explorer',
+			version: '11'
+		},
+		{
+			browserName: 'safari',
+			version: '11.1'
+		},
+		{
+			browserName: 'microsoftedge',
+			version: '17.17134'
+		}
+	]
 };
