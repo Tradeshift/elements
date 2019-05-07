072d1801e5d211048c53a91754a15833d1877cbc
Daniel Spitzer <dsp@tradeshift.com>
2019-05-07 15:53:08 +0200

    test(tooltip): added stories

--- packages/components/tooltip/src/tooltip.js
+++ packages/components/tooltip/src/tooltip.js
@@ -1,39 +1,38 @@
 import { TSElement, unsafeCSS, html } from '@tradeshift/elements';
 import css from './tooltip.css';
 
-customElements.define(
-	'ts-tooltip',
-	class extends TSElement {
-		static get styles() {
-			return [TSElement.styles, unsafeCSS(css)];
-		}
-
-		static get properties() {
-			return {
-				tooltip: { type: String, reflect: true },
-				position: { type: String, reflect: true },
-				width: { type: String, reflect: true }
-			};
-		}
-
-		constructor() {
-			super();
-			this.tooltip = '';
-			this.position = 'right';
-			this.width = 'max-content';
-		}
-
-		render() {
-			return html`
-				<style>
-					:host::before {
-						width: ${this.width};
-					}
-				</style>
-				<div>
-					<slot></slot>
-				</div>
-			`;
-		}
+export class TSTooltip extends TSElement {
+	static get styles() {
+		return [TSElement.styles, unsafeCSS(css)];
 	}
-);
+
+	static get properties() {
+		return {
+			tooltip: { type: String, reflect: true },
+			position: { type: String, reflect: true },
+			width: { type: String, reflect: true }
+		};
+	}
+
+	constructor() {
+		super();
+		this.tooltip = '';
+		this.position = 'right';
+		this.width = 'max-content';
+	}
+
+	render() {
+		return html`
+			<style>
+				:host::before {
+					width: ${this.width};
+				}
+			</style>
+			<div>
+				<slot></slot>
+			</div>
+		`;
+	}
+}
+
+customElements.define('ts-tooltip', TSTooltip);
