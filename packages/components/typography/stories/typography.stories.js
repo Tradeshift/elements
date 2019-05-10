bcdda2d2c6fb68dde705ff7a4664579a68a92675
Kian Esmaeilpour <hossein.esmaeil@tradeshift.com>
2019-05-10 17:01:46 +0200

    Try to bypass storybook

--- packages/components/typography/stories/typography.stories.js
+++ packages/components/typography/stories/typography.stories.js
@@ -6,60 +6,70 @@ storiesOf('ts-typography', module)
 	.add(
 		'Simple',
 		() => html`
+			<div>fake for storybook</div>
 			<ts-typography>Sample Text</ts-typography>
 		`
 	)
 	.add(
 		'color="danger"',
 		() => html`
+			<div>fake for storybook</div>
 			<ts-typography color="danger">Danger Color Text</ts-typography>
 		`
 	)
 	.add(
 		'color="error"',
 		() => html`
+			<div>fake for storybook</div>
 			<ts-typography color="error">Error Color Text</ts-typography>
 		`
 	)
 	.add(
 		'color="success"',
 		() => html`
+			<div>fake for storybook</div>
 			<ts-typography color="success">Success Color Text</ts-typography>
 		`
 	)
 	.add(
 		'color="action"',
 		() => html`
+			<div>fake for storybook</div>
 			<ts-typography color="action">Action Color Text</ts-typography>
 		`
 	)
 	.add(
 		'color="primary"',
 		() => html`
+			<div>fake for storybook</div>
 			<ts-typography color="primary">Primary Color Text</ts-typography>
 		`
 	)
 	.add(
 		'variant="default"',
 		() => html`
+			<div>fake for storybook</div>
 			<ts-typography variant="default">Default Variant Text</ts-typography>
 		`
 	)
 	.add(
 		'variant="title"',
 		() => html`
+			<div>fake for storybook</div>
 			<ts-typography variant="title">Title Variant Text</ts-typography>
 		`
 	)
 	.add(
 		'variant="subtitle"',
 		() => html`
+			<div>fake for storybook</div>
 			<ts-typography variant="subtitle">Subtitle Variant Text</ts-typography>
 		`
 	)
 	.add(
 		'no-wrap',
 		() => html`
+			<div>fake for storybook</div>
 			<ts-typography no-wrap>No Wrap Text</ts-typography>
 		`
 	);
