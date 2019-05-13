6efce437aa15992273e79d45a335e0a04053f5d0
Kian Esmaeilpour <hossein.esmaeil@tradeshift.com>
2019-05-13 12:14:45 +0200

    Add progress bar component

---
+++ packages/components/progress-bar/src/progress-bar.js
@@ -0,0 +1,45 @@
+import { TSElement, unsafeCSS, html } from '@tradeshift/elements';
+import css from './progress-bar.css';
+
+customElements.define(
+	'ts-progress-bar',
+	class extends TSElement {
+		static get styles() {
+			return [TSElement.styles, unsafeCSS(css)];
+		}
+
+		static get properties() {
+			return {
+				total: { type: Number, reflect: true },
+				done: { type: Number, reflect: true },
+				indeterminate: { type: Boolean, reflect: true }
+			};
+		}
+
+		constructor() {
+			super();
+			this.total = 100;
+			this.done = 0;
+			this.indeterminate = false;
+		}
+
+		get doneWidthStyle() {
+			if (!this.indeterminate) {
+				return `width: ${(this.done / this.total) * 100}%`;
+			}
+		}
+
+		render() {
+			return html`
+				<div
+					class="progress-bar-wrapper"
+					?data-indeterminate="${this.indeterminate}"
+				>
+					<div class="progress-bar-total">
+						<div style="${this.doneWidthStyle}" class="progress-bar-done"></div>
+					</div>
+				</div>
+			`;
+		}
+	}
+);
