591173de283a3f97044ac96783131e2fcc3c73e8
Leo Zhang <lza@tradeshift.com>
2019-05-13 15:39:00 +0200

    Update storybook

--- packages/components/button/stories/button.stories.js
+++ packages/components/button/stories/button.stories.js
@@ -1,29 +1,23 @@
 import { storiesOf, html } from '@open-wc/demoing-storybook';
+import { withKnobs, text, select } from '@storybook/addon-knobs';
 import '@tradeshift/elements';
 import '@tradeshift/elements.button';
 
 storiesOf('ts-button', module)
-	.add(
-		'type=""',
-		() => html`
-			<ts-button>Default Button</ts-button>
-		`
-	)
-	.add(
-		'type="primary"',
-		() => html`
-			<ts-button type="primary">Primary Button</ts-button>
-		`
-	)
-	.add(
-		'type="secondary"',
-		() => html`
-			<ts-button type="secondary">Secondary Button</ts-button>
-		`
-	)
-	.add(
-		'type="text"',
-		() => html`
-			<ts-button type="text">Text Button</ts-button>
-		`
-	);
+	.addDecorator(withKnobs)
+	.add('type', () => {
+		const type = select(
+			'Type',
+			{
+				Default: '',
+				Primary: 'primary',
+				Secondary: 'secondary',
+				Text: 'text'
+			},
+			''
+		);
+		const lable = text('Lable', 'Button');
+		return html`
+			<ts-button type="${type}">${lable}</ts-button>
+		`;
+	});
--- packages/components/tooltip/stories/tooltip.stories.js
+++ packages/components/tooltip/stories/tooltip.stories.js
@@ -1,26 +1,24 @@
 import { storiesOf, html } from '@open-wc/demoing-storybook';
+import { withKnobs, text, select } from '@storybook/addon-knobs';
 import '@tradeshift/elements';
 import '@tradeshift/elements.tooltip';
 
-storiesOf('ts-tooltip', module).add(
-	'default',
-	() => html`
-		<style>
-			#root {
-				padding-top: 40px;
-			}
-		</style>
-		<ts-tooltip tooltip="tooltip on the right" position="right">
-			<ts-button>Right</ts-button>
-		</ts-tooltip>
-		<ts-tooltip tooltip="tooltip on the top" position="top">
-			<ts-button>Top</ts-button>
-		</ts-tooltip>
-		<ts-tooltip tooltip="tooltip on the bottom" position="bottom">
-			<ts-button>Bottom</ts-button>
-		</ts-tooltip>
-		<ts-tooltip tooltip="tooltip on the left" position="left">
-			<ts-button>Left</ts-button>
-		</ts-tooltip>
-	`
-);
+storiesOf('Tooltip', module)
+	.addDecorator(withKnobs)
+	.add('Position', () => {
+		const position = select(
+			'Position',
+			['right', 'left', 'top', 'bottom'],
+			'right'
+		);
+		const tooltip = text('Tooltip', 'This is my tooltip');
+		return html`
+			<div
+				style="height:200px; line-height:200px; text-align: center; position:relative"
+			>
+				<ts-tooltip tooltip="${tooltip}" Position="${position}"
+					>Tooltip</ts-tooltip
+				>
+			</div>
+		`;
+	});
