5e84d581d1ea5f1fce03d121f8378e916ea27850
Kian Esmaeilpour <hossein.esmaeil@tradeshift.com>
2019-07-11 17:08:18 +0200

    Test icon

--- packages/components/progress-bar/stories/progress-bar.happo.js
+++ packages/components/progress-bar/stories/progress-bar.happo.js
@@ -1,26 +1,26 @@
-import { storiesOf } from '@open-wc/demoing-storybook';
-import '@tradeshift/elements';
-import '@tradeshift/elements.progress-bar';
-
-import { createHappoStories } from '../../../../.storybook-happo/utils';
-
-storiesOf('ts-progress-bar', module).add('test', () => {
-	const properties = {
-		done: {
-			'0': 0,
-			'5': 5,
-			'100': 100,
-			'600': 600
-		},
-		total: {
-			'10': 10,
-			'1000': 1000
-		}
-	};
-
-	const options = {
-		columns: 2
-	};
-
-	return createHappoStories('progress-bar', properties, '', options);
-});
+// import { storiesOf } from '@open-wc/demoing-storybook';
+// import '@tradeshift/elements';
+// import '@tradeshift/elements.progress-bar';
+//
+// import { createHappoStories } from '../../../../.storybook-happo/utils';
+//
+// storiesOf('ts-progress-bar', module).add('test', () => {
+// 	const properties = {
+// 		done: {
+// 			'0': 0,
+// 			'5': 5,
+// 			'100': 100,
+// 			'600': 600
+// 		},
+// 		total: {
+// 			'10': 10,
+// 			'1000': 1000
+// 		}
+// 	};
+//
+// 	const options = {
+// 		columns: 2
+// 	};
+//
+// 	return createHappoStories('progress-bar', properties, '', options);
+// });
