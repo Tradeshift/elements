884126ce16672ee876eb5c9de499e667aa80762b
Kian Esmaeilpour <hossein.esmaeil@tradeshift.com>
2019-07-12 15:41:55 +0200

    test

--- packages/components/help-text/stories/help-text.happo.js
+++ packages/components/help-text/stories/help-text.happo.js
@@ -1,35 +1,35 @@
-// import { storiesOf } from '@open-wc/demoing-storybook';
-// import '@tradeshift/elements';
-// import '@tradeshift/elements.help-text';
-//
-// import { createHappoStories } from '../../../../.storybook-happo/utils';
-//
-// storiesOf('ts-help-text', module).add('test', () => {
-// 	const properties = {
-// 		rtl: { true: true },
-// 		title: { title: 'Title message' },
-// 		messages: {
-// 			multipleMessage: [
-// 				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus consequatur deserunt excepturi expedita maiores natus, perferendis. Alias assumenda delectus eveniet illo illum, ipsa, laborum ratione reiciendis repellat repellendus totam voluptates!1',
-// 				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. 2',
-// 				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, aspernatur autem consectetur consequuntur dolores dolorum, ducimus exercitationem fuga illum neque nulla placeat provident quas quo suscipit vitae voluptatem voluptates voluptatum. 3'
-// 			]
-// 		},
-// 		size: {
-// 			full: 'full',
-// 			medium: 'medium',
-// 			small: 'small'
-// 		}
-// 	};
-//
-// 	const options = {
-// 		columns: 4,
-// 		persistent_props: {
-// 			messages: [
-// 				'Some very very long long help text to give user more information about the input that they need to provide'
-// 			]
-// 		}
-// 	};
-//
-// 	return createHappoStories('help-text', properties, '', options);
-// });
+import { storiesOf } from '@open-wc/demoing-storybook';
+import '@tradeshift/elements';
+import '@tradeshift/elements.help-text';
+
+import { createHappoStories } from '../../../../.storybook-happo/utils';
+
+storiesOf('ts-help-text', module).add('test', () => {
+	const properties = {
+		rtl: { true: true },
+		title: { title: 'Title message' },
+		messages: {
+			multipleMessage: [
+				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus consequatur deserunt excepturi expedita maiores natus, perferendis. Alias assumenda delectus eveniet illo illum, ipsa, laborum ratione reiciendis repellat repellendus totam voluptates!1',
+				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. 2',
+				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, aspernatur autem consectetur consequuntur dolores dolorum, ducimus exercitationem fuga illum neque nulla placeat provident quas quo suscipit vitae voluptatem voluptates voluptatum. 3'
+			]
+		},
+		size: {
+			full: 'full',
+			medium: 'medium',
+			small: 'small'
+		}
+	};
+
+	const options = {
+		columns: 4,
+		persistent_props: {
+			messages: [
+				'Some very very long long help text to give user more information about the input that they need to provide'
+			]
+		}
+	};
+
+	return createHappoStories('help-text', properties, '', options);
+});
