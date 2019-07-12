ae13bbbf1acc5a0af68fb62ee45489de85f556fb
Kian Esmaeilpour <hossein.esmaeil@tradeshift.com>
2019-07-12 16:34:42 +0200

    test file uploader input

--- packages/components/file-card/stories/file-card.happo.js
+++ packages/components/file-card/stories/file-card.happo.js
@@ -1,30 +1,30 @@
-import { storiesOf } from '@open-wc/demoing-storybook';
-import '@tradeshift/elements';
-import '@tradeshift/elements.file-card';
-
-import { createHappoStories } from '../../../../.storybook-happo/utils';
-
-import { sizes, states } from '../src/utils';
-
-storiesOf('ts-file-card', module).add('test', () => {
-	const properties = {
-		rtl: { true: true },
-		removable: { true: true },
-		state: {
-			[states.DOWNLOAD]: states.DOWNLOAD,
-			[states.FAILED]: states.FAILED
-		},
-		size: sizes
-	};
-
-	const options = {
-		columns: 5,
-		persistent_props: {
-			'file-object': { name: 'fileName.pdf', size: 1234567890 },
-			'error-message': 'Failed to upload',
-			state: states.DOWNLOAD
-		}
-	};
-
-	return createHappoStories('file-card', properties, '', options);
-});
+// import { storiesOf } from '@open-wc/demoing-storybook';
+// import '@tradeshift/elements';
+// import '@tradeshift/elements.file-card';
+//
+// import { createHappoStories } from '../../../../.storybook-happo/utils';
+//
+// import { sizes, states } from '../src/utils';
+//
+// storiesOf('ts-file-card', module).add('test', () => {
+// 	const properties = {
+// 		rtl: { true: true },
+// 		removable: { true: true },
+// 		state: {
+// 			[states.DOWNLOAD]: states.DOWNLOAD,
+// 			[states.FAILED]: states.FAILED
+// 		},
+// 		size: sizes
+// 	};
+//
+// 	const options = {
+// 		columns: 5,
+// 		persistent_props: {
+// 			'file-object': { name: 'fileName.pdf', size: 1234567890 },
+// 			'error-message': 'Failed to upload',
+// 			state: states.DOWNLOAD
+// 		}
+// 	};
+//
+// 	return createHappoStories('file-card', properties, '', options);
+// });
--- packages/components/file-uploader-input/stories/file-uploader-input.happo.js
+++ packages/components/file-uploader-input/stories/file-uploader-input.happo.js
@@ -1,33 +1,33 @@
-// import { storiesOf } from '@open-wc/demoing-storybook';
-// import '@tradeshift/elements';
-// import '@tradeshift/elements.file-uploader-input';
-//
-// import { createHappoStories } from '../../../../.storybook-happo/utils';
-//
-// storiesOf('ts-file-uploader-input', module).add('test', () => {
-// 	const properties = {
-// 		rtl: { true: true },
-// 		disabled: { true: true },
-// 		'help-text-messages': {
-// 			message: [
-// 				'Some very very long long help text to give user more information about the input that they need to provide'
-// 			]
-// 		},
-// 		'hide-file-type-help-text': { true: true },
-// 		size: {
-// 			full: 'full',
-// 			medium: 'medium',
-// 			small: 'small'
-// 		}
-// 	};
-//
-// 	const options = {
-// 		columns: 5,
-// 		persistent_props: {
-// 			'accepted-file-extensions': ['doc', 'pdf', 'jpg'],
-// 			'helpText-title': 'Help Title'
-// 		}
-// 	};
-//
-// 	return createHappoStories('file-uploader-input', properties, '', options);
-// });
+import { storiesOf } from '@open-wc/demoing-storybook';
+import '@tradeshift/elements';
+import '@tradeshift/elements.file-uploader-input';
+
+import { createHappoStories } from '../../../../.storybook-happo/utils';
+
+storiesOf('ts-file-uploader-input', module).add('test', () => {
+	const properties = {
+		rtl: { true: true },
+		disabled: { true: true },
+		'help-text-messages': {
+			message: [
+				'Some very very long long help text to give user more information about the input that they need to provide'
+			]
+		},
+		'hide-file-type-help-text': { true: true },
+		size: {
+			full: 'full',
+			medium: 'medium',
+			small: 'small'
+		}
+	};
+
+	const options = {
+		columns: 5,
+		persistent_props: {
+			'accepted-file-extensions': ['doc', 'pdf', 'jpg'],
+			'helpText-title': 'Help Title'
+		}
+	};
+
+	return createHappoStories('file-uploader-input', properties, '', options);
+});
