0b999e5431d25eeefd7af2c6717a5053ac0eb33f
leo zhang <zdlm@users.noreply.github.com>
2019-08-07 13:20:01 +0200

    Create aside components (#121)

---
+++ packages/components/aside/src/aside.js
@@ -0,0 +1,67 @@
+import { TSElement, unsafeCSS, html, customElementDefineHelper } from '@tradeshift/elements';
+import css from './aside.css';
+import '@tradeshift/elements.button';
+import '@tradeshift/elements.cover';
+
+customElementDefineHelper(
+	'ts-aside',
+	class extends TSElement {
+		static get styles() {
+			return [TSElement.styles, unsafeCSS(css)];
+		}
+
+		static get properties() {
+			return {
+				title: { type: String, attribute: 'data-title' },
+				visible: { type: Boolean, attribute: 'data-visible', reflect: true },
+				hasFoot: { type: Boolean }
+			};
+		}
+
+		constructor() {
+			super();
+			this.title = '';
+			this.visible = false;
+			this.hasFoot = false;
+		}
+
+		close(e) {
+			this.visible = false;
+		}
+
+		footerSlot(e) {
+			this.hasFoot = true;
+		}
+
+		get direction() {
+			return this.bodyDir;
+		}
+
+		get slide() {
+			const pre = 'ts-slide';
+			const mid = this.visible ? 'in' : 'out';
+			const dir = this.direction === 'rtl' ? 'left' : 'right';
+			return `${pre}-${mid}-${dir}`;
+		}
+
+		render() {
+			return html`
+				<div dir="${this.direction}" class="aside-container ${this.slide} ${this.hasFoot ? 'has-footer' : ''}">
+					<header>
+						<div class="aside-title">${this.title}</div>
+						<div class="aside-close">
+							<ts-button class="no-border" icon="remove" @click="${this.close}"></ts-button>
+						</div>
+					</header>
+					<main class="aside-main">
+						<slot name="main"></slot>
+					</main>
+					<footer>
+						<slot name="footer" @slotchange="${this.footerSlot}"></slot>
+					</footer>
+				</div>
+				<ts-cover class="ts-aside-cover" ?data-visible=${this.visible} @click="${this.close}"></ts-cover>
+			`;
+		}
+	}
+);
---
+++ packages/components/aside/stories/aside.stories.js
@@ -0,0 +1,40 @@
+import { storiesOf, html } from '@open-wc/demoing-storybook';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+import '@tradeshift/elements';
+import '@tradeshift/elements.aside';
+
+storiesOf('ts-aside', module)
+	.addDecorator(withKnobs)
+	.add('With footer', () => {
+		const title = text('Title', 'Aside');
+		const visible = boolean('Visible', false);
+		return html`
+			<ts-aside data-title="${title}" ?data-visible="${visible}">
+				<ts-panel slot="main">
+					These oft shamed not mothernot now harolds tear congealed virtues. To hall riot awake parasites ye his
+					mirthful beyond. Such sighed his start glee had soon yes if, domestic days for labyrinth her within in whateer
+					vaunted. Mood few vaunted them hall felt loved than wins muse. A near sight childe ne they departed earthly.
+					Shamed his riot dome for.
+				</ts-panel>
+				<ts-button-group slot="footer">
+					<ts-button type="primary">Button</ts-button>
+				</ts-button-group>
+			</ts-aside>
+		`;
+	})
+	.add('Without footer', () => {
+		const title = text('Title', 'Aside');
+		const visible = boolean('Visible', false);
+		return html`
+			<ts-aside data-title="${title}" ?data-visible="${visible}">
+				<ts-panel slot="main">
+					Forse donne speranza siamo esperienza mentre seguitando sé nostro l'uomo, dare e di da prestasse noi non.
+					Mentre di alcun vita forza siamo sua io in sogiacere, ciascheduna reputiamo primo forse porgiamo dovendo non,
+					beati infiniti nostro sé di dare nostra e a santo, piaceri santo quale ingannati le ripararci carissime, come
+					piene sí noi l'acume siamo cospetto incominciare pregato per, il come in avvien e la, il pieno alcun
+					convenevole e coloro a ammirabile una nostri, bene noia alla alle credere non. Suo la quegli ma nel dio pieno
+					ma transitorie. Di che seguitando purita incominciare convenevole bene, ora.
+				</ts-panel>
+			</ts-aside>
+		`;
+	});
