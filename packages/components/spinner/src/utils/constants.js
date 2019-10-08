ba757c7bc51548e959e5a16f1a527fef6fca8bf4
leo zhang <zdlm@users.noreply.github.com>
2019-10-08 15:47:42 +0200

    Add spinner component (#162)
    * Add spinner component

---
+++ packages/components/spinner/src/utils/constants.js
@@ -0,0 +1,11 @@
+export const sizes = {
+	LARGE: 'large',
+	MEDIUM: 'medium',
+	SMALL: 'small'
+};
+
+export const colors = {
+	BLUE: 'blue',
+	MONO: 'mono',
+	WHITE: 'white'
+};
---
+++ packages/components/spinner/stories/spinner.stories.js
@@ -0,0 +1,36 @@
+import { storiesOf, html } from '@open-wc/demoing-storybook';
+import { withKnobs, text, boolean, select } from '@storybook/addon-knobs';
+import { helpers } from '@tradeshift/elements';
+
+import '@tradeshift/elements.spinner';
+import { sizes, colors } from '../src/utils';
+
+storiesOf('ts-spinner', module)
+	.addDecorator(withKnobs)
+	.add('default', () => {
+		const message = text('Message', 'Loading...');
+		const visible = boolean('Visible', true);
+
+		const size = select(
+			'Size',
+			{
+				default: sizes.LARGE,
+				...helpers.objectKeysChangeCase(sizes)
+			},
+			''
+		);
+
+		const color = select(
+			'Color',
+			{
+				default: colors.BLUE,
+				...helpers.objectKeysChangeCase(colors)
+			},
+			''
+		);
+
+		return html`
+			<ts-spinner ?data-visible="${visible}" data-message="${message}" data-size="${size}" data-color="${color}">
+			</ts-spinner>
+		`;
+	});
