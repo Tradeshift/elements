bf370a53229aad9b1dadb44792a528137ee1a652
Kian Esmaeilpour <hossein.esmaeil@tradeshift.com>
2020-11-20 10:23:17 +0100

    Add success type for dialog

--- packages/components/dialog/src/utils/constants.js
+++ packages/components/dialog/src/utils/constants.js
@@ -1,23 +1,27 @@
 export const dialogTypes = {
 	CONFIRM: 'confirm',
 	WARNING: 'warning',
-	DANGER: 'danger'
+	DANGER: 'danger',
+	SUCCESS: 'success'
 };
 
 export const dialogTypeIcon = {
 	[dialogTypes.CONFIRM]: 'question',
 	[dialogTypes.WARNING]: 'alert',
-	[dialogTypes.DANGER]: 'remove'
+	[dialogTypes.DANGER]: 'remove',
+	[dialogTypes.SUCCESS]: 'check-alt'
 };
 
 export const dialogTypeIconTypes = {
 	[dialogTypes.CONFIRM]: 'primary',
 	[dialogTypes.WARNING]: 'warning',
-	[dialogTypes.DANGER]: 'danger'
+	[dialogTypes.DANGER]: 'danger',
+	[dialogTypes.SUCCESS]: 'success'
 };
 
 export const dialogTypeButtonType = {
 	[dialogTypes.CONFIRM]: 'primary',
 	[dialogTypes.WARNING]: 'warning',
-	[dialogTypes.DANGER]: 'danger'
+	[dialogTypes.DANGER]: 'danger',
+	[dialogTypes.SUCCESS]: 'accept'
 };
