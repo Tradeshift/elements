9cc6822e8caa5f2bf6929e92d3ee6265c6dd5ac0
Aleksandr Elkin <aleksandr.elkin@tradeshift.com>
2021-03-23 18:07:40 +0100

    feat(popover): PEAA-316. Added an initial implementation of popover.

---
+++ packages/components/popover/stories/popover.happo.js
@@ -0,0 +1,62 @@
+import { html } from 'lit-html';
+import '@tradeshift/elements';
+import '@tradeshift/elements.popover';
+
+export default {
+	title: 'ts-popover'
+};
+
+export const AnchoredPosition = () => {
+	return html`
+		<div style="margin-top: 150px; text-align: center; position:relative; width: 100%; height: 300px;">
+			<span id="anchorElem" style="border: 1px solid green;">Anchor element</span>
+		</div>
+		<ts-popover placement="topLeft" header="TopLeft header" anchor="anchorElem" opened>
+			<div slot="content">
+				<p>In popover content you could use html markup, e.g.:</p>
+				<a href="#" style="color: skyblue">link</a>
+			</div>
+		</ts-popover>
+		<ts-popover placement="topRight" header="TopRight header" anchor="anchorElem" opened>
+			<div slot="content">
+				<p>In popover content you could use html markup, e.g.:</p>
+				<a href="#" style="color: skyblue">link</a>
+			</div>
+		</ts-popover>
+		<ts-popover placement="bottomLeft" header="BottomLeft header" anchor="anchorElem" opened>
+			<div slot="content">
+				<p>In popover content you could use html markup, e.g.:</p>
+				<a href="#" style="color: skyblue">link</a>
+			</div>
+		</ts-popover>
+		<ts-popover placement="bottomRight" header="BottomRight header" anchor="anchorElem" opened>
+			<div slot="content">
+				<p>In popover content you could use html markup, e.g.:</p>
+				<a href="#" style="color: skyblue">link</a>
+			</div>
+		</ts-popover>
+	`;
+};
+
+AnchoredPosition.story = {
+	name: 'anchored position'
+};
+
+export const AbsolutelyPositioned = () => {
+	return html`
+		<ts-popover position-left="50px" position-top="calc(50% - 20px)" opened>
+			<div slot="content">
+				<p>In popover content you could use html markup, e.g.:</p>
+				<a href="#" style="color: skyblue">link</a>
+			</div>
+			<div slot="footer">
+				<button>First</button>
+				<button>Second</button>
+			</div>
+		</ts-popover>
+	`;
+};
+
+AbsolutelyPositioned.story = {
+	name: 'absolute position'
+};
