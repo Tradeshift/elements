4a927e622d1022b772127891e98b98c722907134
Aleksandr Elkin <aleksandr.elkin@tradeshift.com>
2022-09-21 17:15:39 +0200

    chore: add the ordering function to storybook preview config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,1 +1,7 @@
 import '@webcomponents/webcomponentsjs';
+
+export const parameters = {
+	options: {
+		storySort: (a, b) => (a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }))
+	}
+};
